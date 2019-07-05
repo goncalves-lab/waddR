b2eec998f5d0c2697b74d19f4c19f5e3d1f8648b
Julian Flesch <julianflesch@gmail.com>
2019-07-05 15:05:30 +0200

    remove obsolete test

--- tests/testthat/test_misc.r
+++ tests/testthat/test_misc.r
@@ -2,18 +2,6 @@ library("testthat")
 library("diffexpR")
 
 
-#### TEST PERMUTATIONS OF ARMADILLO DATA STRUCTURES
-test_that("permutations on cpp stl datatypes",{
-  a <- rnorm(100)
-  n <- 100
-  df <- sapply(1:n, function(e) sample(a, replace = FALSE))
-  
-  permutations_matrix = permutations_internal_test_export(a, n)
-  expect_equal(length(df), length(permutations_matrix))
-  expect_true(all(apply(permutations_matrix, 2, function(perm) {a %in% perm})))
-})
-
-
 #### SANITY CHECK
 test_that("Bogus tests", {
   
