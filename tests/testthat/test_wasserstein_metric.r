0d9c1635ea6beff98c9f8022ecc19205d9e37c83
Julian Flesch <julianflesch@gmail.com>
2019-07-26 15:16:13 +0200

    remove traces of the package transport

--- tests/testthat/test_wasserstein_metric.r
+++ tests/testthat/test_wasserstein_metric.r
@@ -6,70 +6,65 @@ library("waddR")
 ##########################################################################
 
 # R implementation to test against, from the package transport
-if (TRUE) {#!requireNamespace("transport", quietly = TRUE)) {
-  # Copy of the transport implementation of wasserstein metric
-  wasserstein1d <- function (a, b, p = 1, wa = NULL, wb = NULL) {
-    m <- length(a)
-    n <- length(b)
-    stopifnot(m > 0 && n > 0)
-    if (m == n && is.null(wa) && is.null(wb)) {
-      return( mean( abs( sort(b) - sort(a) )^p )^(1/p))
-    }
-    if (is.null(wa)) {
-      wa <- rep(1, m)
-    }
-    if (is.null(wb)) {
-      wb <- rep(1, n)
-    }
-    stopifnot(length(wa) == m && length(wb) == n)
-    
-    # normalizes values to sum up to 1
-    ua <- (wa/sum(wa))[-m]
-    ub <- (wb/sum(wb))[-n]
-    
-    
-    cua <- c(cumsum(ua))
-    cub <- c(cumsum(ub))
-    temp <- cut(cub, breaks = c(-Inf, cua, Inf))
-    arep <- table(temp) + 1
-    temp <- cut(cua, breaks = c(-Inf, cub, Inf))
-    brep <- table(temp) + 1
-    
-    # repeat each element in a and b as often as the intervals in arep and brep are mentionned
-    aa <- rep(sort(a), times = arep)
-    bb <- rep(sort(b), times = brep)
-    
-    # combine ecdf of weights vectors for a and b 
-    uu <- sort(c(cua, cub))
-    
-    ## Quantiles of empirical Fa and Fb
-    uu0 <- c(0, uu)
-    uu1 <- c(uu, 1)
-    areap <- sum((uu1 - uu0) * abs(bb - aa)^p)^(1/p)
-    
-    #cat("a length = ",length(a))
-    #cat(" b length = ",length(b), "\n")
-    #cat("ua length = ", length(ua))
-    #cat(" ub length = ", length(ub), "\n")
-    #cat("cua length = ", length(cua))
-    #cat(" cub length = ", length(cub), "\n")
-    #cat("arep length = ", length(arep))
-    #cat(" brep length = ", length(brep), "\n")
-    #cat("cua  = ", paste(as.character(cua), collapse=", "), "\n")
-    #cat("cub  = ", paste(as.character(cua), collapse=", "), "\n")
-    
-    #cat("a_rep  = ", paste(as.character(arep), collapse=", "), "\n")
-    #cat("b_rep  = ", paste(as.character(brep), collapse=", "), "\n")
-    
-    #cat("aa  = ", paste(as.character(aa), collapse=", "), "\n")
-    #cat("bb  = ", paste(as.character(bb), collapse=", "), "\n")
-    
-    #cat("uu0  = ", paste(as.character(uu0), collapse=", "), "\n")
-    #cat("uu1  = ", paste(as.character(uu1), collapse=", ")+, "\n")
-    return(areap)
+wasserstein1d <- function (a, b, p = 1, wa = NULL, wb = NULL) {
+  m <- length(a)
+  n <- length(b)
+  stopifnot(m > 0 && n > 0)
+  if (m == n && is.null(wa) && is.null(wb)) {
+    return( mean( abs( sort(b) - sort(a) )^p )^(1/p))
   }
-} else {
-  library("transport")
+  if (is.null(wa)) {
+    wa <- rep(1, m)
+  }
+  if (is.null(wb)) {
+    wb <- rep(1, n)
+  }
+  stopifnot(length(wa) == m && length(wb) == n)
+  
+  # normalizes values to sum up to 1
+  ua <- (wa/sum(wa))[-m]
+  ub <- (wb/sum(wb))[-n]
+  
+  
+  cua <- c(cumsum(ua))
+  cub <- c(cumsum(ub))
+  temp <- cut(cub, breaks = c(-Inf, cua, Inf))
+  arep <- table(temp) + 1
+  temp <- cut(cua, breaks = c(-Inf, cub, Inf))
+  brep <- table(temp) + 1
+  
+  # repeat each element in a and b as often as the intervals in arep and brep are mentionned
+  aa <- rep(sort(a), times = arep)
+  bb <- rep(sort(b), times = brep)
+  
+  # combine ecdf of weights vectors for a and b 
+  uu <- sort(c(cua, cub))
+  
+  ## Quantiles of empirical Fa and Fb
+  uu0 <- c(0, uu)
+  uu1 <- c(uu, 1)
+  areap <- sum((uu1 - uu0) * abs(bb - aa)^p)^(1/p)
+  
+  #cat("a length = ",length(a))
+  #cat(" b length = ",length(b), "\n")
+  #cat("ua length = ", length(ua))
+  #cat(" ub length = ", length(ub), "\n")
+  #cat("cua length = ", length(cua))
+  #cat(" cub length = ", length(cub), "\n")
+  #cat("arep length = ", length(arep))
+  #cat(" brep length = ", length(brep), "\n")
+  #cat("cua  = ", paste(as.character(cua), collapse=", "), "\n")
+  #cat("cub  = ", paste(as.character(cua), collapse=", "), "\n")
+  
+  #cat("a_rep  = ", paste(as.character(arep), collapse=", "), "\n")
+  #cat("b_rep  = ", paste(as.character(brep), collapse=", "), "\n")
+  
+  #cat("aa  = ", paste(as.character(aa), collapse=", "), "\n")
+  #cat("bb  = ", paste(as.character(bb), collapse=", "), "\n")
+  
+  #cat("uu0  = ", paste(as.character(uu0), collapse=", "), "\n")
+  #cat("uu1  = ", paste(as.character(uu1), collapse=", ")+, "\n")
+  return(areap)
 }
 
 
