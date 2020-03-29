3f6be4b4dd22967aee7136d44d1d5698921f30a7
RomanSchefzik <51326566+RomanSchefzik@users.noreply.github.com>
2020-03-29 19:26:11 +0200

    some edits

--- R/WassersteinTest.R
+++ R/WassersteinTest.R
@@ -179,7 +179,8 @@
 #' semi-parametric procedure see \code{.wassersteinTestSp}.
 #'
 #'@details Details concerning the testing procedure based on asymptotic theory
-#' can be found in Schefzik et al (2020). \cr Note that the asymptotic theory-based test should only be employed when the two samples \eqn{x} and \eqn{y} can be assumed to come from continuous distributions.
+#' can be found in Schefzik et al (2020). 
+#'  Note that the asymptotic theory-based test should only be employed when the two samples \eqn{x} and \eqn{y} can be assumed to come from continuous distributions.
 #'
 #'@param x sample (vector) representing the distribution of
 #' condition \eqn{A}
@@ -285,7 +286,8 @@
 #'@name wasserstein.test
 #'@details Details concerning the two testing procedures (i.e. the semi-parametric permutation
 #' testing procedure with GPD approximation and the test based on asymptotic theory) can be found in
-#' Schefzik et al. (2020).\cr Note that the asymptotic theory-based test (\code{method="ASY}) should only be employed when the samples \eqn{x} and \eqn{y} can be assumed to come from continuous distributions. In contrast, the semi-parametric test (\code{method="SP"}) can be used for samples coming from continuous or discrete distributions.
+#' Schefzik et al. (2020).
+#'  Note that the asymptotic theory-based test (\code{method="ASY}) should only be employed when the samples \eqn{x} and \eqn{y} can be assumed to come from continuous distributions. In contrast, the semi-parametric test (\code{method="SP"}) can be used for samples coming from continuous or discrete distributions.
 #'
 #'@param x sample (vector) representing the distribution of
 #' condition \eqn{A}
